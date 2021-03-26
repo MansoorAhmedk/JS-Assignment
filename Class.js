d7e94c19180f3f6ea61220cda1792980d2a49d9f
Mansoor-A <69378309+Mansoor-A@users.noreply.github.com>
2021-03-26 11:38:45 +0500

    Add files via upload

---
+++ Class.js
@@ -0,0 +1,20 @@
+// console.log(document);
+// console.log(document.getElementsByTagName('h1'));
+
+// fetching data by Tagname
+var ele=document.getElementsByTagName('h1');
+console.log(ele [0]);
+ele[0].innerText="WebDev"
+console.log(ele[0].innerText);
+
+// fetching data by classname
+var ele=document.getElementsByClassName('webdev');
+console.log(ele [0].type);
+console.log(ele [0].placeholder);
+console.log(ele [0].value);
+ele[0].innerText="Please Enter Text"
+
+
+// fatchig data by Id
+var ele=document.getElementById('python');
+console.log(ele);
